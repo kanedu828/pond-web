99109267d4bb0bba3111ab08159919643f8771c2
Kane Du <45084706+kanedu828@users.noreply.github.com>
2023-03-19 16:39:49 -0400

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -16,7 +16,8 @@ import { Container } from '@mui/material';
 
 const webSocket = io(process.env.REACT_APP_POND_WS_URL || '', {
   withCredentials: true,
-  rejectUnauthorized: false
+  rejectUnauthorized: false,
+  transports: ['websocket']
 });
 
 function Home() {
